da7fd4df90f7f35b3430860144f2d0e5abdc2b24
maharsh-1803 <maharshpatel1803@gmail.com>
2024-05-28 19:28:27 +0530

    task

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import cors from 'cors'
 
 const PORT = process.env.PORT || 5000;
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 dotevn.config();
 
@@ -25,11 +25,11 @@ app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
 
-// app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT,()=>{
